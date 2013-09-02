de42fe5ab7a8399de85a3efd0d60dc26dcae38ef
Dave Fergemann <fergemann@gmail.com>
2013-09-02 22:31:57 +0000

    add node libraries commander, cheerio, restler

--- setup.sh
+++ setup.sh
@@ -27,6 +27,11 @@ sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
+# Install some node libraries for startup assignments
+npm install cheerio
+npm install commander
+npm install restler
+
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
@@ -39,7 +44,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/fergemann/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
